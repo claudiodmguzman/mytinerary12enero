12b28518346bd15cc5dc5b3a7ce3d926a2798226
guzmanclaudiodm@gmail.com <guzmanclaudiodm@gmail.com>
2022-01-14 13:06:29 -0300

    master

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,20 +10,18 @@ const Home = () => {
     return (
 
         <div className="homesweethome">
-            <div className="description">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex excepturi maiores vitae, dolores, suscipit
-                    quasi
-                    natus, laboriosam rerum voluptatum libero accusantium hic. Amet voluptas doloremque sequi, delectus
-                    quidem
-                    eos culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti odio quam nulla impedit
-                    rerum aliquid
-                    totam, delectus ex hic suscipit asperiores doloremque corporis nihil non quo dignissimos animi
-                    perferendis
-                    reprehenderit!. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, nulla ipsa. Dicta enim
-                    molestias laboriosam
-                    tempore soluta. Reiciendis neque nisi dignissimos magni dolorem culpa, aut cumque excepturi id?
-                    Molestias,
-                    nihil.</p>
+
+            <div className='contenedorDescrption'>
+                <div className="description">
+                    <p>Sudamérica es atractiva por diferentes motivos. Su cultura, su gastronomía y su aún menor impacto en el turismo global la hacen un destino atractivo para muchos viajeros que quieren conocer lugares diferentes.</p>
+
+                </div>
+
+                <div className="description">
+                    <p>Para todos aquellos que tengan en mente conocer nuevos destinos, si necesita inspiración para decidir qué viaje realizar, estos son destinos populares de América del Sur que ofrece MyTinerary.</p>
+
+                </div>
+
             </div>
 
             <div className="carrusel">
@@ -38,46 +36,46 @@ const Home = () => {
                     </ol>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img className="d-block w-100" src={imgCity1} height="600" alt="First slide"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>CIUDAD</h5>
-                                    <p>description</p>
-                                </div>
+                            <img className="d-block w-100" src={imgCity1} height="600" alt="First slide" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>CIUDAD</h5>
+                                <p>description</p>
+                            </div>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={imgCity2} height="600" alt="Second slide"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>CIUDAD</h5>
-                                    <p>description</p>
-                                </div>
+                            <img className="d-block w-100" src={imgCity2} height="600" alt="Second slide" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>CIUDAD</h5>
+                                <p>description</p>
+                            </div>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={imgCity3} height="600" alt="Third slide"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>CIUDAD</h5>
-                                    <p>description</p>
-                                </div>
+                            <img className="d-block w-100" src={imgCity3} height="600" alt="Third slide" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>CIUDAD</h5>
+                                <p>description</p>
+                            </div>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={imgCity4} height="600" alt="Third slide"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>CIUDAD</h5>
-                                    <p>description</p>
-                                </div>
+                            <img className="d-block w-100" src={imgCity4} height="600" alt="Third slide" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>CIUDAD</h5>
+                                <p>description</p>
+                            </div>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={imgCity5} height="600" alt="Third slide"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>CIUDAD</h5>
-                                    <p>description</p>
-                                </div>
+                            <img className="d-block w-100" src={imgCity5} height="600" alt="Third slide" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>CIUDAD</h5>
+                                <p>description</p>
+                            </div>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={imgCity6} height="600" alt="Third slide"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>CIUDAD</h5>
-                                    <p>description</p>
-                                </div>
+                            <img className="d-block w-100" src={imgCity6} height="600" alt="Third slide" />
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>CIUDAD</h5>
+                                <p>description</p>
+                            </div>
                         </div>
                     </div>
                     <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
