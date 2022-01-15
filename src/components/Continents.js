580749e78c30b88a85741d6a8b87e370c4bec151
guzmanclaudiodm@gmail.com <guzmanclaudiodm@gmail.com>
2022-01-14 21:27:26 -0300

    master

--- src/components/Continents.js
+++ src/components/Continents.js
@@ -14,6 +14,7 @@ const Country = () => {
             <h1 className="cardMasterTitulo">Continents</h1>
 
             <div className="cardMaster">
+
                 <div className="card">
                     <div className="image">
                         <img src={img1} />
@@ -25,108 +26,6 @@ const Country = () => {
                         <div className="center">
                             <h1>Machu Picchu<br /><span>Perú</span></h1>
                             <p>Es Patrimonio de la Humanidad y una de las nuevas siete maravillas del mundo moderno</p>
-                            <div className="ulCenter">
-                                <ul>
-                                    <li><Linkrouter to="/access">access</Linkrouter></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="image">
-                        <img src={img2} />
-                        <div className="imageTexto">
-                            <p>ECUADOR</p>
-                        </div>
-                    </div>
-                    <div className="details">
-                        <div className="center">
-                            <h1>Chimborazo<br /><span>Ecuador</span></h1>
-                            <p>Medido desde el centro de la tierra, es el punto más lejano del planeta al que puedes llegar sin volar</p>
-                            <div className="ulCenter">
-                                <ul>
-                                    <li><Linkrouter to="/access">access</Linkrouter></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="image">
-                        <img src={img3} />
-                        <div className="imageTexto">
-                            <p>CHILE</p>
-                        </div>
-                    </div>
-                    <div className="details">
-                        <div className="center">
-                            <h1>Desierto de Atacama<br /><span>Chile</span></h1>
-                            <p>Es el mejor sitio del planeta para observar el firmamento</p>
-                            <div className="ulCenter">
-                                <ul>
-                                    <li><Linkrouter to="/access">access</Linkrouter></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="cardMaster">
-                <div className="card">
-                    <div className="image">
-                        <img src={img4} />
-                        <div className="imageTexto">
-                            <p>ARGENTINA</p>
-                        </div>
-                    </div>
-                    <div className="details">
-                        <div className="center">
-                            <h1>Glaciar Perito Moreno<br /><span>Argentina</span></h1>
-                            <p>Se trata del glaciar con más fácil acceso de todo el planeta</p>
-                            <div className="ulCenter">
-                                <ul>
-                                    <li><Linkrouter to="/access">access</Linkrouter></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="image">
-                        <img src={img5} />
-                        <div className="imageTexto">
-                            <p>BRASIL</p>
-                        </div>
-                    </div>
-                    <div className="details">
-                        <div className="center">
-                            <h1>Lençóis Maranhenses<br /><span>Brasil</span></h1>
-                            <p>Se forman centenares de lagunas de color azulado o verdoso, cercadas por dunas de arena blanca</p>
-                            <div className="ulCenter">
-                                <ul>
-                                    <li><Linkrouter to="/access">access</Linkrouter></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="image">
-                        <img src={img6} />
-                        <div className="imageTexto">
-                            <p>VENEZUELA</p>
-                        </div>
-                    </div>
-                    <div className="details">
-                        <div className="center">
-                            <h1>Los Roques<br /><span>Venezuela</span></h1>
-                            <p>Su singularidad y belleza se debe en parte por la formación de una estructura de atolón</p>
                             <div className="ulCenter">
                                 <ul>
                                     <li><Linkrouter to="/access">access</Linkrouter></li>
