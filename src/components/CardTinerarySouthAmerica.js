a611e86a8acdf57e61f90c19ae26e3c7e4c8b374
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-01-18 11:13:53 -0300

    master

--- src/components/CardTinerarySouthAmerica.js
+++ src/components/CardTinerarySouthAmerica.js
@@ -18,9 +18,43 @@ const CardTinerarySouthAmerica = () => {
                 <CardContinents />
             </div>
 
+            <div className="tinerary" >
+                <div className="cardTinerary">
+                    <h1 className="tineraryTitle" >CONTINENT NAME</h1>
 
-            <div className="cardTinerary">
-            SOUTH AMERICA
+                    <div className="tineraryImg" >
+                        IMG
+                    </div>
+
+                    <div className="tineraryInfo" >
+                        <div>
+                            CITY 1
+                        </div>
+
+                        <div>
+                            CITY 2
+                        </div>
+
+                        <div>
+                            CITY 3
+                        </div>
+
+                        <div>
+                            CITY 4
+                        </div>
+
+                        <div>
+                            CITY 5
+                        </div>
+
+                        <div>
+                            CITY 6
+                        </div>
+                    </div>
+
+
+
+                </div>
             </div>
 
         </div>
