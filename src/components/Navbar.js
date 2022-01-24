cafec15659c84cd45719ebde0bb0ba0324855906
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-01-24 13:58:21 -0300

    master

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,23 @@ const Navbar = () => {
     return (
         <div className='navbarContainer'>
 
-            <div className='nochtitulo' >
+            <div className='nochtitulo'>
 
-            <div className="noch">
-                <div className="encabezadoImgLogo">
-                    <Linkrouter to="/"><img src={imgCompass} className="App-logo" height="175" width="175" alt="Logo principal, lleva a la Home page" /></Linkrouter>
+                <div className="noch">
+                    <div className="encabezadoImgLogo">
+                        <Linkrouter to="/"><img src={imgCompass} className="App-logo" height="175" width="175" alt="Logo principal, lleva a la Home page" /></Linkrouter>
+                    </div>
+
+                    <div className="encabezadoNoch">
+                        <div className="smile"></div>
+                    </div>
+
                 </div>
 
-                <div className="encabezadoNoch">
-                    <div className="smile"></div>
+
+                <div className="titulo">
+                    <p>MYTinerary</p>
                 </div>
-
-            </div>
-
-
-            <div className="titulo">
-                <p>MYTinerary</p>
-            </div>
 
             </div>
 
