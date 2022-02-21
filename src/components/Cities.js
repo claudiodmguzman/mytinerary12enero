10132ca8a68dc293baa56cc683869a9c7f3b49cc
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-02-21 13:29:13 -0300

    master

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -3,10 +3,13 @@ import { useParams } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
 import CardCities from './CardCities'
 import { Link as Linkrouter } from 'react-router-dom'
+import searchFilters from './Filtro';
 
 
 function Cities() {
     const [{ cities }, dispatch] = useStateValue()
+    
+    searchFilters(".card-filter",".card")
 
     return (
 
@@ -14,7 +17,7 @@ function Cities() {
 
             <div className='search'>
 
-                <input type="search" name="citySearch" className="formSearch" placeholder="enter the name of the city you want to search" />
+                <input type="search" name="cityCountrySearch" className="formSearch card-filter" placeholder="enter the name of the city you want to search" />
 
             </div>
 
@@ -25,7 +28,7 @@ function Cities() {
             </div>
 
 
-            <div className="master">
+            <div className="master cards">
 
                 <CardCities />
 
