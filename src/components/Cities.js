c926604e143610a38942b52430f556563c337bda
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-02-21 08:47:03 -0300

    master

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -2,16 +2,34 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
 import CardCities from './CardCities'
+import { Link as Linkrouter } from 'react-router-dom'
 
 
-function Cities () {
-    const [{cities}, dispatch] = useStateValue ()
-   
+function Cities() {
+    const [{ cities }, dispatch] = useStateValue()
+
     return (
-        <div className="master">
 
-        <CardCities />
+        <div>
 
+            <div className='search'>
+
+                <input type="text" className="formSearch" placeholder="enter the name of the city you want to search" />
+
+            </div>
+
+            <div className='decoContinents'>
+
+                <Linkrouter className="linkDeco2" to="/continents">...go to Continents</Linkrouter>
+
+            </div>
+
+
+            <div className="master">
+
+                <CardCities />
+
+            </div>
         </div>
     )
 }
--- src/components/Continents.js
+++ src/components/Continents.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardContinents from './CardContinents';
-// import { Link as Linkrouter } from 'react-router-dom';
 
 
 
@@ -13,10 +12,6 @@ const Continents = () => {
 
                 <h1 className="cardMasterTitulo">Continents</h1>
 
-
-                {/* <Linkrouter className="linkDeco" to="/cities">Cities</Linkrouter> */}
-            
-                {/* <Linkrouter className="linkDeco" to="/continents">Continents</Linkrouter> */}
             </nav>
 
             <div>
