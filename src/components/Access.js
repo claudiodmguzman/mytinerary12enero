8a44c2fae46c38f88caf2e0d43d38fc9617373c8
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-02-22 07:59:19 -0300

    master

--- src/components/Access.js
+++ src/components/Access.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 const Access = () => {
 
-    const [itineraries,setItineraries] = useState([])
+    const [itineraries, setItineraries] = useState([])
 
     const [{ cities }, dispatch] = useStateValue()
 
@@ -19,10 +19,10 @@ const Access = () => {
     useEffect(() => {
         citiesSelecter.map(city =>
             axios.get(`http://localhost:4000/api/itinerarios/${city.name}`)
-                .then(response => setItineraries(response.data.response.itinerario) 
+                .then(response => setItineraries(response.data.response.itinerario)
                 )
         )
-        
+
     }, [])
 
 
@@ -37,40 +37,38 @@ const Access = () => {
                 <div className='access' >
 
 
-                    <div className="accessFondo">{city.name}</div>
+                    <div className="accessFondo">City: {city.name}</div>
 
-                    {/* <div className="accessFondo">{cities.country}</div>
+                    <div className="accessFondo">Country: {city.country}</div>
 
-                        <div className="accessFondo">{cities.currency}</div>
+                    <div className="accessFondo">Currency: {city.currency}</div>
 
-                        <div className="accessFondo">{cities.language}</div>
+                    <div className="accessFondo">Lenguege: {city.language}</div>
 
-                        <div className="accessFondo">{cities.demonym}</div>
+                    <div className="accessFondo">Demonym: {city.demonym}</div>
 
-                        <div className="accessFondo">{cities.timezone}</div>
+                    <div className="accessFondo">Timezone: {city.timezone}</div>
 
-                        <div className="accessFondo">{cities.population}</div> */}
+                    <div className="accessFondo">Population: {city.population}</div>
 
                     {/* <div className="accessImage">
-                            <img className="d-block w-100 accessImage" src={process.env.PUBLIC_URL + `/imgCiudades/${cities.image}`} height="600" alt="img6City-Republica-Checa-Praga.jpg" />
-                        </div>
+                        <img className="d-block w-100 accessImage" src={process.env.PUBLIC_URL + `/imgCiudades/${cities.image}`} height="600" alt="img6City-Republica-Checa-Praga.jpg" />
+                    </div> */}
 
 
-                        <div className="accessFondo accessdet">{cities.textShort}</div>
+                    <div className="accessDetail" >
+                        <Accordion title='Expand Detail'>
+                            <p className='paccessDetail' >{city.description}</p></Accordion>
+                    </div>
 
-                        <div className="accessDetail" >
-                            <Accordion title='Expand Detail'>
-                                <p className='paccessDetail' >{cities.description}</p></Accordion>
-                        </div>
 
-                        
-                        <div className="accessFondo">{cities.coordinates}</div>
+                    <div className="accessFondo">Coodinates: {city.coordinates}</div>
 
-                        <div className="accessFondo">{cities.googlemaps}</div>
-                        
-                        <div className="accessFondo">{cities.continents}</div>
+                    <div className="accessFondo">Google Maps: {city.googlemaps}</div>
 
-                        <div className="accessFondo">{cities.region}</div> */}
+                    <div className="accessFondo">Continents: {city.continents}</div>
+
+                    <div className="accessFondo">Region: {city.region}</div>
 
 
                     <div className='accessCommLik' >
@@ -84,15 +82,15 @@ const Access = () => {
                 </div>
             )
 
-            
+
 
             }
-
+{/* 
             {itineraries.map(itinerarie =>
-                
+
                 <div className="accessFondo">{itinerarie.nameTour}</div>
 
-                )}
+            )} */}
 
         </div>
 
