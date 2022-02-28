6510f7570ad2190ef810469b17638fc1a9b26942
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-02-28 14:27:17 -0300

    master

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose") // va a almacenar los datos de las ciudades "cities"
+
+const itinerariesSchema = new mongoose.Schema ({
+    firstName:{type:String,require:true},
+    lastName:{type:String,require:true},
+    email:{type:String,require:true},
+    password:{type:String,require:true},
+})
+
+const Itinerario = mongoose.model("itineraries",itinerariesSchema)
+
+module.exports = Itinerario;
--- src/components/CardSignUp.js
+++ src/components/CardSignUp.js
@@ -6,33 +6,45 @@ const CardSignUp = () => {
 
         <div className='signUpInContainer'>
 
-            
 
-                <form className="signUpIn">
 
-                    <label className="texSign">Before using our services you need to create an account</label>
+            <form className="signUpIn">
 
-                    <div className="form-group">
+                <label className="texSign">Before using our services you need to create an account</label>
 
-                        <input type="email" className="form-control formSign" id="exampleDropdownFormEmail1"
-                            required placeholder="email@example.com" />
-                    </div>
-                    <div className="form-group">
+                <div className="form-group">
 
-                        <input type="password" className="form-control formSign" id="exampleDropdownFormPassword1"
-                            required placeholder="Password" />
-                    </div>
-                    <div className="form-group">
+                    <input type="text" className="form-control formSign" id="exampleDropdownFormPassword1"
+                        required placeholder="First Name" />
+                </div>
 
-                        <input type="password" className="form-control formSign" id="exampleDropdownFormPassword1"
-                            required placeholder="Repeat Password" />
-                    </div>
+                <div className="form-group">
 
-                    <button type="submit" className="btn btn-success">Sign Up</button>
+                    <input type="text" className="form-control formSign" id="exampleDropdownFormPassword1"
+                        required placeholder="Last Name" />
+                </div>
 
-                </form>
-            </div>
-       
+                <div className="form-group">
+
+                    <input type="email" className="form-control formSign" id="exampleDropdownFormEmail1"
+                        required placeholder="email@example.com" />
+                </div>
+                <div className="form-group">
+
+                    <input type="password" className="form-control formSign" id="exampleDropdownFormPassword1"
+                        required placeholder="Password" />
+                </div>
+                <div className="form-group">
+
+                    <input type="password" className="form-control formSign" id="exampleDropdownFormPassword1"
+                        required placeholder="Repeat Password" />
+                </div>
+
+                <button type="submit" className="btn btn-success">Sign Up</button>
+
+            </form>
+        </div>
+
 
 
     )
