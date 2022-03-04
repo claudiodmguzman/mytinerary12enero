f84c27a158936484edd02bd1545ff13e4f6cac32
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-03-04 16:46:57 -0300

    MASTER

--- backend/controllers/validator.js
+++ backend/controllers/validator.js
@@ -5,24 +5,24 @@ const validator = (req,res,next) => {
     console.log.apply(req.body.NuevoUsuario)
     const Schema = joi.object({
         firstName:joi.string().max(10).min(3).trim().pattern(new RegExp("[a-zA-Z]")).required().messages({
-            "string.min":"Nombre: el nombre debe contener al menos 3 (tres) caracteres",
-            "string.empty":"Nombre: el campo no puede estar vacío"
+            "string.min":"The first name must contain at least 3 (three) characters",
+            "string.empty":"First name: the field cannot be empty",
         }),
 
         lastName:joi.string().max(20).min(3).trim().pattern(new RegExp("[a-zA-Z]")).required().messages({
-            "string.min":"Apellido: el apellido debe contener al menos 3 (tres) caracteres",
-            "string.empty":"Apellido: el campo no puede estar vacío",
+            "string.min":"The last name must contain at least 3 (three) characters",
+            "string.empty":"Last name: the field cannot be empty",
         }),
 
         email:joi.string().email({minDomainSegments:2}).required().messages({
-            "string.email":"Correo: formato de correo electónico no válido",
-            "string.empty":"Correo: el campo no puede estar vacío",
+            "string.email":"Invalid e-mail format",
+            "string.empty":"E-mail: the field cannot be empty",
         }),
 
         password:joi.string().max(30).min(6).trim().pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/).required().messages({
-            "string.empty":"Contraseña: el campo no puede estar vacío",
-            "string.pattern.base":"Contraseña: la contraseña debe contener como mínimo una mayúscula, una minúscula y un número",
-            "string.min":"Contraseña: la constraseña debe contener al menos 6 (seis) caracteres",
+            "string.empty":"Password: the field cannot be empty",
+            "string.pattern.base":"The password must contain at least one uppercase letter, one lowercase letter and one number",
+            "string.min":"The password must contain at least 6 (six) characters",
         }),
 
     })
