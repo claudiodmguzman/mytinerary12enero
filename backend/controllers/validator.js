85b028b596872eed6e25a4e99e947b96f024e60c
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-03-13 16:06:20 -0300

    master

--- backend/controllers/validator.js
+++ backend/controllers/validator.js
@@ -1,38 +1,38 @@
-const joi = require ("joi")
+const joi = require("joi")
 
-const validator = (req,res,next) => {
+const validator = (req, res, next) => {
 
     console.log.apply(req.body.NuevoUsuario)
     const Schema = joi.object({
-        firstName:joi.string().max(10).min(3).trim().pattern(new RegExp("[a-zA-Z]")).required().messages({
-            "string.min":"The first name must contain at least 3 (three) characters",
-            "string.empty":"First name: the field cannot be empty",
+        firstName: joi.string().max(10).min(3).trim().pattern(new RegExp("[a-zA-Z]")).required().messages({
+            "string.min": "The first name must contain at least 3 (three) characters",
+            "string.empty": "First name: the field cannot be empty",
         }),
 
-        lastName:joi.string().max(20).min(3).trim().pattern(new RegExp("[a-zA-Z]")).required().messages({
-            "string.min":"The last name must contain at least 3 (three) characters",
-            "string.empty":"Last name: the field cannot be empty",
+        lastName: joi.string().max(20).min(3).trim().pattern(new RegExp("[a-zA-Z]")).required().messages({
+            "string.min": "The last name must contain at least 3 (three) characters",
+            "string.empty": "Last name: the field cannot be empty",
         }),
 
-        email:joi.string().email({minDomainSegments:2}).required().messages({
-            "string.email":"Invalid e-mail format",
-            "string.empty":"E-mail: the field cannot be empty",
+        email: joi.string().email({ minDomainSegments: 2 }).required().messages({
+            "string.email": "Invalid e-mail format",
+            "string.empty": "E-mail: the field cannot be empty",
         }),
 
-        password:joi.string().max(50).min(6).trim().pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/).required().messages({
-            "string.empty":"Password: the field cannot be empty",
-            "string.pattern.base":"The password must contain at least one uppercase letter, one lowercase letter and one number",
-            "string.min":"The password must contain at least 6 (six) characters",
+        password: joi.string().max(50).min(6).trim().pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/).required().messages({
+            "string.empty": "Password: the field cannot be empty",
+            "string.pattern.base": "The password must contain at least one uppercase letter, one lowercase letter and one number",
+            "string.min": "The password must contain at least 6 (six) characters",
         }),
 
         google: joi.boolean
 
     })
 
-    const validation = Schema.validate(req.body.NuevoUsuario,{abortEarly:true})
+    const validation = Schema.validate(req.body.NuevoUsuario, { abortEarly: true })
 
     if (validation.error) {
-        return res.json({success:"falseVAL",response:validation})
+        return res.json({ success: "falseVAL", response: validation })
     }
 
     next()
--- src/components/CardSignUp.js
+++ src/components/CardSignUp.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import React from 'react';
 import swal from 'sweetalert';
-import GoogleLogin from 'react-google-login';
+//import GoogleLogin from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 
 
 
 const CardSignUp = () => {
 
-    const responseGoogle = async (response) => {
+    function responseGoogle(response) {
         console.log(response);
     }
 
@@ -16,7 +17,7 @@ const CardSignUp = () => {
         // lastName: response.profileObj.familyName,
         // email: response.profileObj.email,
         // password: response.googleId+"Ab",
-        google: true,
+        // google: true,
     }
 
 
@@ -28,7 +29,7 @@ const CardSignUp = () => {
             lastName: event.target[1].value,
             email: event.target[2].value,
             password: event.target[3].value,
-            google: true
+            //google: true
         }
 
 
@@ -40,22 +41,27 @@ const CardSignUp = () => {
             )
 
         function displayMessages(data) {
-            if (data.success = "false") {
-                console.log(data.response.error.details)
-                data.response.error.details.map(error => swal(error.message))
-            }
-            else if (data.success = "false") {
-                console.log(swal(data.response))
-            }
-            else if (data.success = "false") {
-                console.log(swal(data.response))
-            }
-            else if (data.success = "true") {
-                console.log(swal(data.response))
-            }
+            console.log(data.response.error.details)
+            data.response.error.details.map(error => swal(error.message))
         }
 
-        
+        // function displayMessages(data) {
+        // if (data.success = false) {
+        // console.log(data.response.error.details)
+        // data.response.error.details.map(error => swal(error.message))
+        // }
+        // else if (data.success = false) {
+        //     console.log(swal(data.response))
+        // }
+        // else if (data.success = false) {
+        //     console.log(swal(data.response))
+        // }
+        // else if (data.success = true) {
+        //     console.log(swal(data.response))
+        // }
+        // }
+
+
     }
 
     return (
