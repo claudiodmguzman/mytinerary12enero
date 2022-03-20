9ff016366e593a76376ba0276f8c5f1fbbaf272f
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-03-19 21:27:35 -0300

    master

--- src/components/CardContinents.js
+++ src/components/CardContinents.js
@@ -17,25 +17,26 @@ import img6 from './img/imgContinentes/img6-Oceania.png';
 
 
 
-
 const CardContinents = () => {
+
+  const [{ filterCity }, dispatch] = useStateValue()
 
   //const [{ filterCity }, { filterContinent }, dispatch] = useStateValue()
   //const [itineraries, setCities] = useState([])
   //const [{ cities }, dispatch] = useStateValue()
 
-  const [continenties, setCities] = useState([])
-  const [{ cities }] = useStateValue()
-  const { id } = useParams()
-  const continentSelecter = cities.filter(continents => continents._id === id)
+  // const [continenties, setCities] = useState([])
+  // const [{ cities }] = useStateValue()
+  // const { id } = useParams()
+  // const continentSelecter = cities.filter(continents => continents._id === id)
 
-  useEffect(() => {
-    continentSelecter.map(continents =>
-      axios.get(`http://localhost:4000/api/datos/${continents.name}`)
-        .then(response => setCities(response.data.response.cityCont)
-        )
-    )
-  }, [])
+  // useEffect(() => {
+  //   continentSelecter.map(continents =>
+  //     axios.get(`http://localhost:4000/api/datos/${continents.name}`)
+  //       .then(response => setCities(response.data.response.cityCont)
+  //       )
+  //   )
+  // }, [])
 
   return (
 
@@ -43,33 +44,39 @@ const CardContinents = () => {
     <div>
       {/* <h1 className="cardMasterTitulo">Continents</h1> */}
 
-      {/* {continentSelecter.map((continents) => */}
+      <div className='ContinentsContainer'>
 
-        <div className='ContinentsContainer'>
+        <div className='cardContinentsContainer'>
 
-          <div className='cardContinentsContainer'>
+          <div className='imgCardContinent'>
+            <Linkrouter to="/cardTineraryAfrica"><img src={img1} alt="imagen del continente en forma de globo terraqueo" /></Linkrouter>
+            {/* <Linkrouter className='item-Continent' to={`/access/${continents._id}`}><img className="accessImage" src={process.env.PUBLIC_URL + `/imgContinentesGlobo/${city.imageGlobo}`} alt="image of city" /></Linkrouter> */}
+          </div>
 
-            {/* <div className='imgCardContinent'>
-              <Linkrouter to="/cardTinerarySouthAmerica"><img src={process.env.PUBLIC_URL + `/imgContinentesGlobo/${""continente"".image}`} alt="imagen del continente en forma de globo terraqueo" /></Linkrouter>
-              </div> */}
+          <Accordion title="{nameContinent}">
 
-            <Accordion title="Render Name Continent">
-              <>
-                {cities.map((cityCont) => (
-                  <ul className='cardUlContinent'>
-                    <li><Linkrouter className='item-Continent' to={`/access/${cityCont._id}`}>{cityCont.name}</Linkrouter></li>
-                  </ul>
-                ))}
-              </>
-            </Accordion>
+            <>
+              {filterCity?.map((city) => (
 
-          </div >
+                <ul className='cardUlContinent'>
+                  <li><Linkrouter className='item-Continent' to={`/access/${city._id}`}>{city.name}</Linkrouter></li>
+                </ul>
 
-        </div>
+              ))}
+            </>
 
-      {/* )} */}
+          </Accordion>
+
+
+
+
+
+        </div >
+
+      </div>
 
     </div>
+
 
   )
 }
--- src/components/Continents.js
+++ src/components/Continents.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 
 const Continents = () => {
 
-    
+    const [{ cities }, dispatch] = useStateValue()
 
     
     return (
