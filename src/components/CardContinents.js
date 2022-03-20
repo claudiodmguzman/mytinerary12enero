9a6db027c11cb93bf66a15aeda9d8e5889a33cc8
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-03-20 15:03:46 -0300

    master

--- src/components/CardContinents.js
+++ src/components/CardContinents.js
@@ -4,13 +4,6 @@ import { useStateValue } from '../StateProvider';
 import { Accordion } from 'react-bootstrap-accordion';
 import 'react-bootstrap-accordion/dist/index.css';
 
-import img1 from './img/imgContinentes/img1-America-del-Sur.png';
-import img2 from './img/imgContinentes/img2-America-del-Norte.png';
-import img3 from './img/imgContinentes/img3-Africa.png';
-import img4 from './img/imgContinentes/img4-Asia.png';
-import img5 from './img/imgContinentes/img5-Europa.png';
-import img6 from './img/imgContinentes/img6-Oceania.png';
-
 
 const CardContinents = () => {
 
@@ -20,34 +13,39 @@ const CardContinents = () => {
 
    return (
 
-      <>
-         {filterContynente?.map((contynente) => (
 
-            <div>
-               {/* <h1 className="cardMasterTitulo">Continents</h1> */}
-               <div className='ContinentsContainer'>
+
+      <div>
+
+         {/* <h1 className="cardMasterTitulo">Continents</h1> */}
+         < div className='ContinentsContainer' >
+            <>
+               {filterContynente?.map((contynente) => (
+
                   <div className='cardContinentsContainer'>
 
                      <div className='imgCardContinent'>
-                        <Linkrouter className='item-Continent' to="/CardTineraryAfrica"><img className='imgCardContinent' src={process.env.PUBLIC_URL + `/imgContinentesGlobo/${contynente.imageGlobo}`} alt="Imagen Globo del Continente" /></Linkrouter>
+                        <Linkrouter to="/CardTineraryAfrica"><img src={process.env.PUBLIC_URL + `/imgContinentesGlobo/${contynente.imageGlobo}`} alt="Imagen Globo del Continente" /></Linkrouter>
                      </div>
 
                      <Accordion title={contynente.nameContinents}>
-                       <>
+                        <>
                            {filterCity?.map((cityContinente) => (
                               <ul className='cardUlContinent'>
                                  <li><Linkrouter className='item-Continent' to={`/access/${cityContinente._id}`}>{cityContinente.name}</Linkrouter></li>
                               </ul>
                            ))}
-                        </> 
+                        </>
                      </Accordion>
 
                   </div >
-               </div>
-            </div>
+               ))}
+            </>
+         </div>
 
-         ))}
-      </>
+      </div >
+
+
 
    )
 }
