176c974ca7248ce933faa5ae5ca35bdabfb563a4
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-04-26 20:30:52 -0300

    master

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -7,12 +7,37 @@ import axios from 'axios';
 
 function Likes(props) {
 
-    const [{ user }, dispatch] = useStateValue()
+    const likes = props.itinerario
+
+    const [{ cities }, dispatch] = useStateValue()
+    const [{ user }] = useStateValue()
+ 
+    const [itineraries, setItineraries] = useState([])
+ 
+    const { id } = useParams()
+    const citiesSelecter = cities.filter(city => city._id === id)
+ 
+    useEffect(() => {
+       citiesSelecter.map(city =>
+          axios.get(`http://localhost:4000/api/itinerarios/${city.name}`)
+             .then(response => setItineraries(response.data.response.itinerario)
+             )
+       )
+    }, [])
+
+    // const [{ user }, dispatch] = useStateValue()
 
     // const [like, setLike] = useState(0)
 
     const likeDisLike = async () => {
-        await axios.put(`http://localhost:4000/api/likeDisLike/${user.id}`)
+        const token = localStorage.getItem("token")
+        console.log(user)
+        console.log(props)
+        await axios.put(`http://localhost:4000/api/likeDisLike/${user.datosUser.id}`,{}, {
+            headers: {
+              'Authorization': 'Bearer ' + token // método de autorizacion estandar para autoriza y autentificar al usuario
+            }
+          })
         .then(response => console.log(response))
     }
   
