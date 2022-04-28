d2d9e883c6bc9ca3eb0c788f52fa4e71a72ea8f8
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-04-27 23:43:37 -0300

    master

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useStateValue } from '../StateProvider';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import imgCompassLog from './img/imgNavbar/imgCompassLog.png';
+import imgCompassLogOut from './img/imgNavbar/imgCompassLogOut.png';
 
 
 
@@ -11,18 +13,18 @@ function Likes(props) {
 
     const [{ cities }, dispatch] = useStateValue()
     const [{ user }] = useStateValue()
- 
+
     const [itineraries, setItineraries] = useState([])
- 
+
     const { id } = useParams()
     const citiesSelecter = cities.filter(city => city._id === id)
- 
+
     useEffect(() => {
-       citiesSelecter.map(city =>
-          axios.get(`http://localhost:4000/api/itinerarios/${city.name}`)
-             .then(response => setItineraries(response.data.response.itinerario)
-             )
-       )
+        citiesSelecter.map(city =>
+            axios.get(`http://localhost:4000/api/itinerarios/${city.name}`)
+                .then(response => setItineraries(response.data.response.itinerario)
+                )
+        )
     }, [])
 
     // const [{ user }, dispatch] = useStateValue()
@@ -33,15 +35,15 @@ function Likes(props) {
         const token = localStorage.getItem("token")
         console.log(user)
         console.log(props)
-        await axios.put(`http://localhost:4000/api/likeDisLike/${Likes}`,{}, {
+        await axios.put(`http://localhost:4000/api/likeDisLike/${Likes}`, {}, {
             headers: {
-              'Authorization': 'Bearer ' + token // método de autorizacion estandar para autoriza y autentificar al usuario
+                'Authorization': 'Bearer ' + token // método de autorizacion estandar para autoriza y autentificar al usuario
             }
-          })
-        // .then(response => console.log(response))
-        .then(response => setLikes(response.data.response.length))
+        })
+            // .then(response => console.log(response))
+            .then(response => setLikes(response.data.response.length))
     }
-  
+
 
 
     return (
@@ -51,7 +53,8 @@ function Likes(props) {
             <div className="accessLikes">
 
 
-                <button className="accessLikes" onClick={likeDisLike}>♠{likes}</button>
+                <button className="paccessLikesRojo" onClick={likeDisLike}>❤ {likes}</button>
+
 
 
             </div>
